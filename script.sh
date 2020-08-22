4e5fe8e6ef3ac56702c9b08b957de07aa9b3f427
BrunoAFK <bruno@pavelja.me>
2020-08-22 20:35:52 +0200

    Adding script for siwtching primary monitor in Ubuntu 20.04

--- script.sh
+++ script.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 MONITOR_LIST=$(xrandr -q | grep -w "connected" | grep -v "primary" | awk '{print $1}')
 MONITOR_PRIM=$(xrandr -q | grep -w "primary" | awk '{print $1}')
 echo "Primary monitor is $MONITOR_PRIM
-
 rows=$(echo $MONITOR_LIST | sed -e $'s/,/\\\n/g')
 spaces=$(echo "${rows}" | sed -e 's/ //g')
 first=$(echo "${spaces}" | head -n 1)
-
 xrandr --output $first --primary
-
 echo "$first is new primary monitor"
